31b8d8ca9a689efda2347b4e199da1f0732c2587
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2024-05-18 23:51:04 +0330

    created common headers

---
+++ feaplusplus/include/common/common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <vector>
+#include <iostream>
+
+typedef double Real;
+typedef std::vector<Real> Vector;
+typedef std::vector<Vector> Matrix;
+
+#endif // COMMON_H
