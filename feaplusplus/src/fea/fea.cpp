629e84cec41f546698471a2f93585959d7c7a057
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2024-05-25 17:29:08 +0330

    improved fea module

--- feaplusplus/src/fea/fea.cpp
+++ feaplusplus/src/fea/fea.cpp
@@ -1,17 +1,28 @@
 #include "fea/fea.h"
 
-FEA::FEA() {
+FEA::FEA() : solver(nullptr) {}
+
+void FEA::setSolver(Solver* solver) {
+    this->solver = solver;
 }
 
 void FEA::setupModel(const Mesh& mesh) {
     this->mesh = mesh;
+    mesh.generateStiffnessMatrix();
 }
 
 void FEA::analyze() {
+    if (solver) {
+        solver->solve(mesh);
+    } else {
+        std::cerr << "No solver set!" << std::endl;
+    }
 }
 
 void FEA::visualizeResults() {
+    postprocessor.visualize(mesh);
 }
 
 void FEA::generateReport() {
+    postprocessor.generateReport(mesh);
 }
